d3b1d00f5a6612fb333be52743803d4d34184c53
Manseong Han <akstjd31@naver.com>
2026-01-13 14:39:43 +0900

    feat : GameManager.cs 생성
    - 심플 플레이어 생성
    - 플레이어 PV 컴포넌트 추가

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Photon.Pun;
+using System.Collections;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public static GameManager Instance;
+    [SerializeField] private Transform playerPrefab;
+    [SerializeField] private Vector3 spawnPos;
+    
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        StartCoroutine(SpawnPlayerWhenConnected());
+    }
+
+    IEnumerator SpawnPlayerWhenConnected()
+    {
+        yield return new WaitUntil(() => PhotonNetwork.InRoom);
+        PhotonNetwork.Instantiate(playerPrefab.name, spawnPos, Quaternion.identity);
+    }
+}
