f53fc89ee5b1bdc15f972b90698b512e3d131f2f
Manseong Han <akstjd31@naver.com>
2026-01-13 16:48:59 +0900

    fixed : 인터페이스 수정
    - 변수, 메서드 추가

--- Assets/Scripts/Interaction/IInteractable.cs
+++ Assets/Scripts/Interaction/IInteractable.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 
 public interface IInteractable
 {
-    void RequestInteract(int actorId);
+    int ViewId { get; }                 // 해당 ViewID
+    string Prompt { get; }              // 화면에 띄울 문구
+    bool CanInteract(int actorId);      // 현 상호작용이 가능한지? (왜냐면 다른 플레이어가 사용중일수도)
+    void ServerInteract(int actorId);   // 상태 변경
+    void ClientApplyState();            // 변경된 상태를 클라이언트에게 전달
 }
