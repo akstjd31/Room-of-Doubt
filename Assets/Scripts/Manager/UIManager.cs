37e7a0f0cce699a8f8196fc0161da73072b515ae
Manseong Han <akstjd31@naver.com>
2026-01-13 21:49:11 +0900

    chore : UIManager.cs 주석 추가

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
+// 일단 텍스트 출력용 관리자인데 나중에 바뀔 수 있음.
 public class UIManager : Singleton<UIManager>
 {
     [SerializeField] private TextMeshProUGUI objNameText;
