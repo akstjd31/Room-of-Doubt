d7e64320506d42cf8632a0f08280cba433510423
Manseong Han <akstjd31@naver.com>
2026-01-14 10:03:24 +0900

    feat : 선택된 슬롯 시각화
    - 배경 색 변경으로 현 포커싱된 슬롯 확인

--- Assets/Scripts/Manager/QuickSlotManager.cs
+++ Assets/Scripts/Manager/QuickSlotManager.cs
@@ -8,6 +8,7 @@ public class QuickSlotManager : MonoBehaviourPun
     const int MAX_SLOT_COUNT = 4;
 
     [Header("Slot")]
+    [SerializeField] private GameObject slot;
     public Slot[] slots;
     public int selectedSlotIndex;
 
@@ -16,11 +17,25 @@ public class QuickSlotManager : MonoBehaviourPun
 
     private void Awake()
     {
+        if (!photonView.IsMine) return;
+
         playerInput = this.GetComponent<PlayerInput>();
         selectAction = playerInput.actions["Select"];
         scrollAction = playerInput.actions["Scroll"];
 
         slots = new Slot[MAX_SLOT_COUNT];
+
+        if (slot != null)
+        {
+            GameObject quickSlotObj = GameObject.FindGameObjectWithTag("QuickSlot");
+
+            for (int i = 0; i < MAX_SLOT_COUNT; i++)
+            {
+                GameObject newSlotObj = Instantiate(slot, quickSlotObj.transform.GetChild(0));
+                slots[i] = newSlotObj.GetComponent<Slot>();
+            }
+        }
+
         selectedSlotIndex = -1;
     }
 
@@ -41,12 +56,25 @@ public class QuickSlotManager : MonoBehaviourPun
             scrollAction.performed -= OnScrollSlotPerformed;
     }
 
+    private void UpdateSlotFocusing(int index)
+    {
+        for (int i = 0; i < MAX_SLOT_COUNT; i++)
+        {
+            if (i == index)
+                slots[i].backgroundImage.color = Color.green;
+            else
+                slots[i].backgroundImage.color = Color.black;
+        }
+    }
+
     private void OnSelectSlotPerformed(InputAction.CallbackContext ctx)
     {
         int nextIndex = int.Parse(ctx.control.name) - 1;
         
         if (nextIndex < 0 || nextIndex >= MAX_SLOT_COUNT) return;
         selectedSlotIndex = nextIndex;
+
+        UpdateSlotFocusing(selectedSlotIndex);
     }
 
     private void OnScrollSlotPerformed(InputAction.CallbackContext ctx)
@@ -54,7 +82,12 @@ public class QuickSlotManager : MonoBehaviourPun
         float value = ctx.ReadValue<Vector2>().y;
         
         int delta = value > 0 ? 1 : -1;
-        int nextIndex = selectedSlotIndex + delta < 0 ? MAX_SLOT_COUNT - 1 : 0;
+        int nextIndex = selectedSlotIndex + delta;
+
+        if (nextIndex < 0) nextIndex = MAX_SLOT_COUNT - 1;
+        else if (nextIndex >= MAX_SLOT_COUNT) nextIndex = 0;
+        
         selectedSlotIndex = nextIndex;
+        UpdateSlotFocusing(selectedSlotIndex);
     }
 }
--- Assets/Scripts/Slot/Slot.cs
+++ Assets/Scripts/Slot/Slot.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
+
+// 들어온 정보, 초기화만 해줌.
 public class Slot : MonoBehaviour
 {
     public Item currentItem;
     public Image iconImage;
+    public Image backgroundImage;
 
     public void AddItem(Item newItem)
     {
         currentItem = newItem;
         iconImage.sprite = newItem.itemIcon;
-        iconImage.enabled = true;
     }
 
     public void ClearSlot()
     {
         currentItem = null;
-        iconImage.enabled = false;
+        iconImage.sprite = null;
     }
 }
