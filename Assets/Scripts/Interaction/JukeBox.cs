63c785f33bbb40eb80d12157d569bf7f3a569dc6
Manseong Han <akstjd31@naver.com>
2026-01-20 16:54:50 +0900

    feat : JukeBox.cs, VendingMachine.cs 생성
    - 각 상호작용 시 카메라 전환

---
+++ Assets/Scripts/Interaction/JukeBox.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+
+public class JukeBox : InteractableBase
+{
+    public override void Interact(int actorNumber)
+    {
+        
+    }
+
+    protected override IEnumerator InitRoutine()
+    {
+        yield break;
+    }
+}
