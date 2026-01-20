5a72f2eacae9c3787c0aa6f2a10d1514cadcbfe6
Manseong Han <akstjd31@naver.com>
2026-01-20 14:15:59 +0900

    feat : 키패드 구현

--- Assets/Scripts/KeyPad/KeyPadManager.cs
+++ Assets/Scripts/KeyPad/KeyPadManager.cs
@@ -1,26 +1,28 @@
 using System;
-using System.Linq;
+using System.Collections.Generic;
 using UnityEngine;
-
+using WebSocketSharp;
+using TMPro;
 
 public class KeyPadManager : MonoBehaviour
 {
     const int MAX_CODE_LENGTH = 4;
     [SerializeField] int[] codes;
-    [SerializeField] private int currentNum;
-    private string screenText;
-    private string answer;
+    [SerializeField] private int currentNumLength;
+    [SerializeField] private string collect;
+    [SerializeField] private TMP_Text screenText;
+    private string input;
     private string result;
 
     private void Start()
     {
         codes = new int[MAX_CODE_LENGTH];
-        currentNum = 0;
+        input = "";
+        currentNumLength = 0;
     }
 
     private void Update()
     {
-        screenText = string.Join("", codes.Select(i => i.ToString()).ToArray());
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -29,9 +31,41 @@ public class KeyPadManager : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, 10))
             {
-                if (currentNum < Convert.ToInt32(MAX_CODE_LENGTH))
+                if (currentNumLength < Convert.ToInt32(MAX_CODE_LENGTH))
                 {
-                    
+                    string nStr = hit.transform.gameObject.GetComponent<Number>().NumStr;
+
+                    if (nStr.IsNullOrEmpty()) return;
+
+                    int n;
+                    if (int.TryParse(nStr, out n))
+                    {
+                        codes[currentNumLength] = n;
+                        input += nStr;
+                        currentNumLength++;
+                    }
+                }
+                
+                screenText.text = input;
+                if (currentNumLength >= Convert.ToInt32(MAX_CODE_LENGTH))
+                {
+                    result = String.Join("", new List<int>(codes).ConvertAll(i => i.ToString()).ToArray());
+                    Debug.Log(result);
+
+                    if (collect.Equals(result))
+                    {
+                        Debug.Log("정답!");
+                        // 맞췄을 때 해야할 부분
+                    }
+                    else
+                    {
+                        currentNumLength = 0;
+                        
+                        for (int i = 0; i < codes.Length; i++)
+                            codes[i] = 0;
+                    }
+
+                    input = "";
                 }
             }
         }
